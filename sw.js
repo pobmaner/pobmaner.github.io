15180a43b8b46a2188a6e7500ec833ea94ccefc3
pobmaner <2472547361@qq.com>
2025-08-16 12:42:27 +0800

    Site updated: 2025-08-16 12:42:26

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1755318670092";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1755319345982";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
